016e1eab68beee9cc21715263ddd40e8c74bfdd8
Mohammad Hassieb <mohammadhassieb@gmail.com>
2023-09-10 21:17:44 +0300

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,23 +1,32 @@
-from fabric.api import local
-from datetime import datetime
+#!/usr/bin/python3
+""" Based on '1-pack_web_static.py'. It distributes an archive to my web
+    servers using the function 'do_deploy'.
+"""
+from fabric.api import env, put, run, sudo
+from os.path import exists
+env.hosts = ['54.210.88.216', '100.25.104.112']
+env.user = 'ubuntu'
 
-def do_pack():
-    """
-    Create a compressed archive of the web_static folder
-    """
 
-    # Create the current date and time string for the archive name
-    now = datetime.now()
-    archive_name = "web_static_{}{}{}{}{}{}.tgz".format(
-        now.year, now.month, now.day, now.hour, now.minute, now.second)
+def do_deploy(file_path):
+    """ It distributes an archive to my web servers."""
+    if not exists(file_path):
+        print('file doesnt exist')
+        return False
+    try:
+        filename = file_path.split('/')[-1]
+        file = filename.split('.')[0]
+        put(file_path, '/tmp/')
+        sudo('mkdir -p /data/web_static/releases/{}'.format(file))
+        sudo('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
+             .format(filename, file))
+        run('rm -f /tmp/{}'.format(filename))
+        run('rm -f /data/web_static/current')
+        run("ln -sf /data/web_static/releases/{}/web_static/ \
+                /data/web_static/current".format(file))
 
-    # Create the versions directory if it doesn't exist
-    local("mkdir -p versions")
-
-    # Compress the web_static folder into the archive
-    result = local("tar -cvzf versions/{} web_static".format(archive_name))
-
-    # Check if the compression was successful
-    if result.failed:
-        return None
-    return "versions/{}".format(archive_name)
+        return True
+    except Exception as e:
+        print(e)
+        return False
+    
