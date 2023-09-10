baf44dc0c53cce429b5e4cc2868f825984a707f3
Mohammad Hassieb <mohammadhassieb@gmail.com>
2023-09-10 21:42:46 +0300

    Update 100-clean_web_static.py

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Fabfile to delete out-of-date archives."""
+# Fabfile to delete out-of-date archives.
 import os
 from fabric.api import *
 
@@ -12,7 +12,9 @@ def do_clean(number=0):
     Args:
         number (int): The number of archives to keep.
 
-    If number is 0 or 1, keeps only the most recent archive.
+    If number is 0 or 1, keeps only the most recent archive. If
+    number is 2, keeps the most and second-most recent archives,
+    etc.
     """
     number = 1 if int(number) == 0 else int(number)
 
